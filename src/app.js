b78f914c621f7413e9bf316867cbfe0aa64cd8d2
nawaid <hasantauqeer20@gmail.com>
2024-07-01 17:29:09 +0530

    include cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const connectDB = require("./db/conn");
 require("dotenv").config();
+const cors = require("cors");
 const Query = require("./model/queryModel");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
+app.use(cors());
 
 const startServer = async () => {
   try {
